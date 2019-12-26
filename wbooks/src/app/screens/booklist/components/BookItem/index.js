5fb148b2623273eda10c2597b25d42d6a4214e04
Pato <patricio.fernandez@wolox.com.ar>
2019-12-26 14:00:28 -0300

    more corrections

--- wbooks/src/app/screens/booklist/components/BookItem/index.js
+++ wbooks/src/app/screens/booklist/components/BookItem/index.js
@@ -6,14 +6,14 @@ class BookItem extends Component {
 
 	render() {
 		return (
-            <View style={styles.container}>
-                <Image style={styles.image}
-                source={{uri: 'http://wolox-training.s3.amazonaws.com/uploads/6942334-M.jpg'}} />
-                <View style={styles.textContainer}>
-                    <Text style={styles.bookTitle}>A Little Bird Told Me</Text>    
-                    <Text style={styles.bookDescription}>Timothy Cross</Text>    
-              </View>
-            </View>
+      <View style={styles.container}>
+        <Image style={styles.image}
+        source={{uri: 'http://wolox-training.s3.amazonaws.com/uploads/6942334-M.jpg'}} />
+        <View style={styles.textContainer}>
+          <Text style={styles.bookTitle}>A Little Bird Told Me</Text>    
+          <Text style={styles.bookDescription}>Timothy Cross</Text>    
+        </View>
+      </View>
 		);
 	}
 }
