e9f84df29af9d00122e37ac4f56239c381ddb847
Pato <patricio.fernandez@wolox.com.ar>
2019-12-26 12:47:19 -0300

    more corrections

--- wbooks/src/app/screens/booklist/index.js
+++ wbooks/src/app/screens/booklist/index.js
@@ -1,24 +1,17 @@
 import React, { Component } from 'react';
 import { View, StyleSheet } from 'react-native';
 import BookItem from './components/BookItem';
+import styles from './styles';
 
 class BookList extends Component {
 
 	render() {
 		return (
 			<View style={styles.container}>
-                <BookItem />
+        <BookItem />
 			</View>
 		);
 	}
 }
-
-const styles = StyleSheet.create({
-	container: {
-		flex: 1,
-        alignItems: 'center',
-        padding:20,
-	}
-});
 
 export default BookList;
