27a25c3eeee1bbc2498af89f36c6187916a47ce3
Pato <patricio.fernandez@wolox.com.ar>
2020-01-10 14:30:26 -0300

    corrections

--- wbooks/src/app/screens/Settings/index.js
+++ wbooks/src/app/screens/Settings/index.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
+
 import styles from './styles';
 
-const SettingsScreen = () => (
-  <View style={styles.container}>
-    <Text>Settings!</Text>
-  </View>
-);
+function SettingsScreen () {
+  return (
+    <View style={styles.container}>
+      <Text>Settings!</Text>
+    </View>
+  )
+};
 
 export default SettingsScreen;
